760d8b2f9ac645e44e617d8281438916515bc4f4
Kyle McKee <mckee.kyle@gmail.com>
2021-05-27 08:10:37 -0400

    set up tests and got one to fail.

---
+++ test/testAdoption.test.js
@@ -0,0 +1,22 @@
+const { assert } = require("chai");
+
+const Adoption = artifacts.require("Adoption");
+
+contract("Adoption", (accounts) => {
+  let adoption;
+  let expectedPetId;
+
+  before(async () => {
+    adoption = await Adoption.deployed();
+  });
+
+  describe("adopting a pet and retrieving account addresses", async () => {
+    before("adopt a pet using accounts[0]", async () => {
+      await adoption.adopt(8, { from: accounts[0] });
+      expectedAdopter = accounts[0];
+    });
+    it("fails", () => {
+      assert.fail();
+    });
+  });
+});
